1847673ea3e468345ddca8d556de7e872d885ce1
Diego Mejia <diegoamejia.10@gmail.com>
2022-10-31 21:48:15 -0400

    Fix typo

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,7 +9,7 @@ function Home() {
       <div className='home--sections'>
         <Section className='home__characters' title='characters' image='https://i.postimg.cc/nzBLYWYF/if-21-harry-potter-colour-glasses-and-scar-2730334-88126.png'/>
         <Section className='home__students' title='students' image='https://i.postimg.cc/Bnz2nXqd/hogwarts-logo.png'/>
-        <Section className='home__proffesors' title='proffesors' image='https://i.postimg.cc/zf9BhNh2/if-19-harry-potter-colour-harrys-wand-2730332-88138.png' />
+        <Section className='home__professors' title='professors' image='https://i.postimg.cc/zf9BhNh2/if-19-harry-potter-colour-harrys-wand-2730332-88138.png' />
         <Section className='home__houses' title='houses' image='https://i.postimg.cc/L8QhHL8p/if-06-harry-potter-colour-sorting-hat-2730319-88134.png' />
       </div>
     </div>
