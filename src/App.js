af487464279a531b523d3823e6ad021ab1c55bfc
Diego Mejia <diegoamejia.10@gmail.com>
2022-11-01 22:23:05 -0400

    Add routes and pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
-import Home from './pages/Home/Home';
 import {Routes, Route, Link} from 'react-router-dom';
 import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
+import Characters from './pages/Characters/Characters'
+import Houses from './pages/Houses/Houses'
+import Professors from './pages/Professors/Professors'
+import Students from './pages/Students/Students'
+
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='characters' element={<Characters/>}/>
+        <Route path='professors' element={<Professors/>}/>
+        <Route path='houses' element={<Houses/>}/>
+        <Route path='students' element={<Students/>}/>
       </Routes>
     </div>
   );
