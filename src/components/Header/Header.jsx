3dbb7bd37cbbe61d26ad791a5beef7cf4c5528af
Diego Mejia <diegoamejia.10@gmail.com>
2023-01-08 19:45:36 -0400

    Add Home design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Header.css'
+import Navbar from '../Navbar/Navbar'
 function Header() {
   return (
     <header className='header'>
         <img alt="logo-hogwarts" className='header--logo' src='https://i.postimg.cc/6Q7qTq8G/if-14-harry-potter-colour-hogwarts-shield-2730327-88145.png'/>
          <h1 className='header--title'>HOGWARTS</h1>
+        <Navbar/>
     </header>
   )
 }
